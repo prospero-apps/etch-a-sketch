c5e7f75b0ef7a9242a6f4b3b843078c522ee899d
Kamil <prospero.apps@gmail.com>
2022-07-10 17:11:13 +0200

    Add colors to cells

--- js-file.js
+++ js-file.js
@@ -6,11 +6,11 @@ const containerSize = 800;
 const button = document.getElementById('resolution');
 button.addEventListener('click', () => {
     let res = prompt("How many squares per side (max. 100)",
-                     "Enter a value between 2-100.");
-    
+        "Enter a value between 2-100.");
+
     // min and max square count
-    if(res < 2) res = 2;
-    if(res > 100) res = 100;
+    if (res < 2) res = 2;
+    if (res > 100) res = 100;
 
     // remove all from container and create new grid
     container.replaceChildren();
@@ -53,21 +53,53 @@ function createCell(cellSize) {
 
     // control painting ability
     let paint = true;
+    let cellPainted = false;
+    let timesPainted = 0;
 
     // paint cell when mouse enters
-    cell.addEventListener('mouseenter', () => {
-        if(paint) cell.classList.add('painted');
+    let red, green, blue;
+    let redComponent, greenComponent, blueComponent;
+    let newRed, newGreen, newBlue;
+
+    cell.addEventListener('mouseenter', () => {       
+        if (!cellPainted) {
+            red = Math.floor(Math.random() * 256)
+            green = Math.floor(Math.random() * 256)
+            blue = Math.floor(Math.random() * 256)
+
+            redComponent = Math.floor(red / 10);
+            greenComponent = Math.floor(green / 10);
+            blueComponent = Math.floor(blue / 10);
+
+            let cellColor = `rgb(${red}, ${green}, ${blue})`;
+            cell.style.backgroundColor = cellColor;
+            cellPainted = true;
+        }
+        else {                                
+            if(newRed !== 0 || newGreen !== 0 || newBlue !== 0) {
+                timesPainted++;
+                newRed = red - redComponent * timesPainted;
+                newGreen = green - greenComponent * timesPainted;
+                newBlue = blue - blueComponent * timesPainted;
+
+                if(newRed < 0) newRed = 0;
+                if(newGreen < 0) newGreen = 0;
+                if(newBlue < 0) newBlue = 0;
+
+                let cellColor = `rgb(${newRed}, ${newGreen}, ${newBlue})`;
+                cell.style.backgroundColor = cellColor;
+            }            
+        }
     });
- 
+
     // left-click to disable painting
     container.addEventListener('click', (e) => {
-        if(e.button == 0) {
+        if (e.button == 0) {
             paint = !paint;
         }
     })
- 
+
     return cell;
 }
 
-// createGrid(16, 16, '45px');
 createGrid(16);
